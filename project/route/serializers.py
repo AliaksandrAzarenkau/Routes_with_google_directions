c7bca11ec2c0b5d93f1014945935b056a99740a1
AliaksandrAzarenkau <136499379+AliaksandrAzarenkau@users.noreply.github.com>
2024-02-03 14:31:47 +0300

    Added route archive

--- project/route/serializers.py
+++ project/route/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Route
+from client.models import Client, ClientObjectsProfile
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
@@ -13,8 +14,6 @@ class CartAddProductSerializer(serializers.Serializer):
 class RouteCreateSerializer(serializers.Serializer):
 
     def create(self, validated_data):
-        print(validated_data.get('email'))
-        print(validated_data.get('client_ids'))
         response = Route.objects.create(
             email=validated_data.get('email'),
             client_ids=validated_data.get('client_ids'),
@@ -24,4 +23,52 @@ class RouteCreateSerializer(serializers.Serializer):
 
     class Meta:
         model = 'Route'
+        fields = ['email', 'client_ids', 'created']
+
+
+class RouteArchiveSerializer(serializers.Serializer):
+    email = serializers.CharField(label='Пользователь')
+    client_ids = serializers.CharField(label="Список ID объектов")
+    created = serializers.DateTimeField(label='Дата создания маршрута')
+
+    def get_details(self, pk):
+        """Достаём все адреса объектов маршрута"""
+        route = Route.objects.get(id=pk)
+        client_data = ClientObjectsProfile.objects.all()
+        ids = route.client_ids
+
+        objects_list = []
+        filtered_ids = ids.split(',')
+        for obj_id in filtered_ids:
+            for i in obj_id:
+                obj = ClientObjectsProfile.objects.get(id=int(i))
+                objects_list.append(obj)
+
+        # objects_list = []
+        # one_route_addresses = []
+        # temp_list = []
+        # for route in routes_data:
+        #     ids = route.client_ids
+        #     filtered_ids = ids.split(',')
+        #     for obj_id in filtered_ids:
+        #         for i in obj_id:
+        #             obj = ClientObjectsProfile.objects.get(id=int(i))
+        #             objects_list.append(obj)
+
+        #         for i in obj_id:
+        #             address = ''
+        #             client = client_data.get(id=int(i))
+        #             city = client.city
+        #             street = client.street
+        #             building = client.building
+        #             address = f'{city}, {street}, {building}'
+        #             temp_list.append(address)
+        #     one_route_addresses.append(temp_list)
+        #     temp_list = []
+        # objects_list.append(one_route_addresses)
+
+        return objects_list
+
+    class Meta:
+        model = Route
         fields = ['email', 'client_ids', 'created']
